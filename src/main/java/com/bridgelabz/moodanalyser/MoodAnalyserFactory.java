692aa34ccda40905edbb121969f82e8ad88c36ac
pushkarpm24 <pushkarmorey555@gmail.com>
2020-04-03 16:56:28 +0530

    ADDED:code to moodanalyser object with default constructor

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyserFactory.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyserFactory.java
@@ -1,19 +1,31 @@
 package com.bridgelabz.moodanalyser;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class MoodAnalyserFactory {
     //Default Constructor
-    public static Constructor getConstructor(String className , Class... parameter) throws MoodAnalysisException {
+    public static Constructor getConstructor(String className, Class... parameter) throws MoodAnalysisException {
         try {
             Class<?> moodAnalyserClass = Class.forName(className);
             return moodAnalyserClass.getConstructor(parameter);
+        } catch (ClassNotFoundException e) {
+            throw new MoodAnalysisException("class not found", MoodAnalysisException.ExceptionType.CLASS_NOT_FOUND);
+        } catch (NoSuchMethodException e) {
+            throw new MoodAnalysisException("method not found", MoodAnalysisException.ExceptionType.METHOD_NOT_FOUND);
         }
-        catch (ClassNotFoundException e) {
-            throw new MoodAnalysisException("class not found",MoodAnalysisException.ExceptionType.CLASS_NOT_FOUND);
+    }
+
+    public static MoodAnalyser createMoodAnalyserObject(Constructor constructor) {
+        try {
+            return (MoodAnalyser) constructor.newInstance();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
         }
-        catch (NoSuchMethodException e) {
-            throw new MoodAnalysisException("method not found",MoodAnalysisException.ExceptionType.METHOD_NOT_FOUND);
-        }
+        return null;
     }
 }
